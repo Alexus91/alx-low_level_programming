173746aac2b6c73ecaf5c42a8298559c17f2bb47
kamal fadl <mrfadlkamal@gmail.com>
2023-03-16 18:35:33 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
  * main - prints " Programming is like building a multilingual puzzle "
- * return 0 .
+ * Return : always 0.
  */
 
 int main(void)
 {
-	puts("programming is like building a multilingual puzzle");
-	return(0) ;
+	puts("programming is like building a multilingual puzzle);
+	return (0);
 }
