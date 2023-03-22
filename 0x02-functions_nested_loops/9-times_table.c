7cf7e2239c771dfacbb143c4b71a3fc8a9f027bc
kamal fadl <mrfadlkamal@gmail.com>
2023-03-22 11:04:24 -0700

    fix betty warning in 8. I'm federal agent Jack Bauer, and today is the longest day of my life

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * times_table - Write a function that prints the 9 times table
+ * Return: empty output
+ */
+void times_table(void)
+{
+	int x, y, z, u, d;
+	for (x = 0 , x <= 9; x++)
+	{
+	for (y =
