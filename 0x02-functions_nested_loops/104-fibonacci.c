10400320ca5ab042dc828c7c8dbc3af78b5ce92e
kamal fadl <mrfadlkamal@gmail.com>
2023-03-22 18:34:37 -0700

    104 fibonacci fix long int issue

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,14 +11,14 @@ int main(void)
 	int count;
 	long long x1 = 1, x2 = 2, x3;
 
-	printf("%d, %d", x1, x2);
+	printf("%lld, %lld", x1, x2);
 
 	for (count = 3; count <= 98; count++)
 	{
 		x3 = x1 + x2;
 		x1 = x2;
 		x2 = x3;
-		printf(", %d", x3);
+		printf(", %lld", x3);
 	}
 	printf("\n");
 	return (0);
