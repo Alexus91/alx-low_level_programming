c8f431253a00dba52107f135ac13bfff591d94e0
Alexus91 <mrfadlkamal@gmail.com>
2023-04-27 19:43:58 -0700

    5. The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void first(void) __attribute__ ((constructor));
+/**
+ * fisrt - print a sentence
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
